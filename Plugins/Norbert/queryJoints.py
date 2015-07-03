68246d2d8b5251761dd3c1b374d097816e1112bf
Gereon Fox <s9gefoxx@stud.uni-saarland.de>
2015-07-03 13:24:22 +0200

    Made script output more compact.

--- Plugins/Norbert/queryJoints.py
+++ Plugins/Norbert/queryJoints.py
@@ -29,17 +29,17 @@ ALMEMORY_KEY_NAMES = []
 
 for j in joints:
 	ALMEMORY_KEY_NAMES.append("Device/SubDeviceList/{j}/Position/Sensor/Value".format(j=j))
-	ALMEMORY_KEY_NAMES.append("Device/SubDeviceList/{j}/Position/Actuator/Value".format(j=j))
+	#ALMEMORY_KEY_NAMES.append("Device/SubDeviceList/{j}/Position/Actuator/Value".format(j=j))
 
 import os
 import sys
 import time
-
+import itertools
 from naoqi import ALProxy
 
 memory = ALProxy("ALMemory", "127.0.0.1", 9559)
 
 def query():
-	for key in ALMEMORY_KEY_NAMES:
-		print key, memory.getData(key)
+	for (label, key) in itertools.izip(joints, ALMEMORY_KEY_NAMES):
+		print label, memory.getData(key)
 
