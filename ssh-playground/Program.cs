ac5e897c390650e8602721e62b4e79abc21b3052
Gereon Fox <s9gefoxx@stud.uni-saarland.de>
2015-07-03 13:46:46 +0200

    Created a playground project for the ssh connection library.

---
+++ ssh-playground/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Renci.SshNet;
+
+namespace sshplayground
+{
+	class MainClass
+	{
+
+		private static SshClient client;
+		private static System.IO.MemoryStream input = new System.IO.MemoryStream();
+		private static System.IO.MemoryStream output = new System.IO.MemoryStream();
+		private static System.IO.TextWriter inputWriter;
+		private static System.IO.TextReader outputReader;
+		private static Shell shell;
+
+		private static void connect(string hostName, string userName, string password)
+		{
+			if (client != null && client.IsConnected)
+				return;
+
+			client = new SshClient(new PasswordConnectionInfo(hostName, userName, password));
+			shell = client.CreateShell(input, output, null);
+
+			shell.Start();
+
+			inputWriter.WriteLine("python");
+
+			using (var sr = new System.IO.StreamReader("queryJoints.py"))
+			{
+				inputWriter.Write(sr.ReadToEnd());
+			}
+
+			outputReader.ReadToEnd();
+		}
+
+		private static void queryJoints()
+		{
+			inputWriter.WriteLine("query()");
+
+			var data = outputReader.ReadToEnd().Split(null);
+
+			Console.WriteLine(data);
+		}
+
+		public static void Main (string[] args)
+		{
+			inputWriter = new System.IO.StreamWriter(input);
+			outputReader = new System.IO.StreamReader(output);
+
+			connect("localhost", "nao", "nao");
+			queryJoints();
+		}
+	}
+}
