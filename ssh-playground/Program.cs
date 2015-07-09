8f6f5d95564eeb69c19783465b36ab9320c1019a
Gereon Fox <s9gefoxx@stud.uni-saarland.de>
2015-07-09 14:34:25 +0200

    Set up a dummy project to make SSH communication work (kind-of...)

--- ssh-playground/Program.cs
+++ ssh-playground/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Renci.SshNet;
+using Renci.SshNet.Common;
 
 namespace sshplayground
 {
@@ -7,48 +8,85 @@ namespace sshplayground
 	{
 
 		private static SshClient client;
-		private static System.IO.MemoryStream input = new System.IO.MemoryStream();
-		private static System.IO.MemoryStream output = new System.IO.MemoryStream();
-		private static System.IO.TextWriter inputWriter;
-		private static System.IO.TextReader outputReader;
-		private static Shell shell;
+		private static System.IO.TextWriter input;
+		private static System.IO.TextReader output;
+		private static ShellStream sshStream;
+
+
+		private static void sendReceive(string toSend)
+		{
+			input.WriteLine(toSend);
+			input.Flush();
+			System.Threading.Thread.Sleep(1000);
+			Console.Write(output.ReadToEnd());
+		}
+
 
 		private static void connect(string hostName, string userName, string password)
 		{
 			if (client != null && client.IsConnected)
 				return;
 
-			client = new SshClient(new PasswordConnectionInfo(hostName, userName, password));
+
+			var connectionInfo = new KeyboardInteractiveConnectionInfo(hostName, userName);
+			connectionInfo.AuthenticationPrompt += delegate(object sender, AuthenticationPromptEventArgs e)
+			{
+				foreach (var prompt in e.Prompts)
+					prompt.Response = password;
+			};
+
+			client = new SshClient(connectionInfo);
+			client.Connect();
+
+
+			sshStream = client.CreateShellStream("", 80, 40, 80, 40, 1024);
+
+
+			input = new System.IO.StreamWriter(sshStream);
+			output = new System.IO.StreamReader(sshStream);
+
+			sendReceive("echo Hallo");
+
+
+			/*
+			using (var sr = new System.IO.StreamReader("queryJoints.py"))
+				sendReceive(sr.ReadToEnd());
+			*/
+
+			/*
 			shell = client.CreateShell(input, output, null);
 
 			shell.Start();
 
 			inputWriter.WriteLine("python");
 
+			inputWriter.Flush();
+
 			using (var sr = new System.IO.StreamReader("queryJoints.py"))
 			{
 				inputWriter.Write(sr.ReadToEnd());
 			}
 
-			outputReader.ReadToEnd();
+			inputWriter.Flush();
+
+			Console.WriteLine(outputReader.ReadToEnd());
+			*/
 		}
 
 		private static void queryJoints()
 		{
-			inputWriter.WriteLine("query()");
+			input.WriteLine("query()");
 
-			var data = outputReader.ReadToEnd().Split(null);
+			var data = output.ReadToEnd().Split(null);
 
 			Console.WriteLine(data);
 		}
 
 		public static void Main (string[] args)
 		{
-			inputWriter = new System.IO.StreamWriter(input);
-			outputReader = new System.IO.StreamReader(output);
 
 			connect("localhost", "nao", "nao");
-			queryJoints();
+			//queryJoints();
 		}
 	}
 }
