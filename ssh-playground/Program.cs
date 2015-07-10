8fabe17debc35d1a2c5610a0a23e00174a24fdd7
Gereon Fox <s9gefoxx@stud.uni-saarland.de>
2015-07-10 13:20:16 +0200

    This works most smoothly with the robot :-)

--- ssh-playground/Program.cs
+++ ssh-playground/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Renci.SshNet;
 using Renci.SshNet.Common;
+using System.Text.RegularExpressions;
 
 namespace sshplayground
 {
@@ -11,15 +12,56 @@ namespace sshplayground
 		private static ShellStream sshStream;
 
 
-		private static string command(string cmd, int timeout=500)
+		private static Regex shellTerm = new Regex("\\$");
+		private static Regex pythonTerm = new Regex("\\>\\>\\>|\\.\\.\\.");
+		private static bool shellReady = false;
+		private static bool pythonReady = false;
+
+
+		private static string command(string cmd, Regex terminator, Regex readyTerminator=null)
 		{
+			if (readyTerminator!=null)
+				Console.Write(sshStream.Expect(readyTerminator));
+
 			sshStream.WriteLine(cmd);
-			System.Threading.Thread.Sleep(timeout);
-			string s = sshStream.Read();
-			Console.Write(s);
-			return s;
+
+			if (terminator == null)
+				return null;
+
+			string output = sshStream.Expect(terminator);
+			Console.Write(output);
+
+			var s = output.IndexOf('\n');
+			var e = output.LastIndexOf('\n');
+
+			output = output.Substring(s + 1, e - s);
+
+			return output;
 		}
 
+		private static string shellCommand(string cmd)
+		{
+			return shellCommand(cmd, shellTerm);
+		}
+
+		private static string shellCommand(string cmd, Regex terminator)
+		{
+			var r = command(cmd, terminator, shellReady ? null : shellTerm);
+			shellReady = true;
+			return r;
+		}
+
+		private static string pythonCommand(string cmd)
+		{
+			return pythonCommand(cmd, pythonTerm);
+		}
+
+		private static string pythonCommand(string cmd, Regex terminator)
+		{
+			var r = command(cmd, terminator, pythonReady ? null : pythonTerm);
+			pythonReady = true;
+			return r;
+		}
 
 		private static void connect(string hostName, string userName, string password)
 		{
@@ -37,27 +79,28 @@ namespace sshplayground
 			client = new SshClient(connectionInfo);
 			client.Connect();
 
-
 			sshStream = client.CreateShellStream("", 80, 40, 80, 40, 1024);
 
-			System.Threading.Thread.Sleep(500);
-			Console.Write(sshStream.Read());
-
-			command("python");
+			shellCommand("python", null);
 
 			using (var sr = new System.IO.StreamReader("queryJoints.py"))
 			{
 				String line;
 				while ((line = sr.ReadLine()) != null)
-					command(line);
+					pythonCommand(line);
 			}
 		}
 
 		private static void queryJoints()
 		{
-			var data = command("query()");
+			string line;
 
-			Console.WriteLine(data);
+			using (var sr = new System.IO.StringReader(pythonCommand("query()")))
+				while ((line = sr.ReadLine()) != null)
+				{
+					var data = line.Trim().Split();
+					Console.WriteLine("{0} = {1}", data[0], data[1]);
+				}
 		}
 
 		public static void Main (string[] args)
